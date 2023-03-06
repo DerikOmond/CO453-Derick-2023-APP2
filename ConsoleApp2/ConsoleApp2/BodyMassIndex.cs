304fa66e65bd36617b975973961b217e0782e89d
DerikOmond <derik.omond@gmail.com>
2023-03-06 00:04:32 +0000

    Asking Measurments
    One method used to bring together 2 separate measurements.
    Asks the wight and height of the user.

--- ConsoleApp2/ConsoleApp2/BodyMassIndex.cs
+++ ConsoleApp2/ConsoleApp2/BodyMassIndex.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    internal class BodyMassIndex
+    public class BodyMassIndex
     {
         public void Run()
         {
             DisplayHeader();
+            AskMeasurments();
+
+            Console.ReadLine();
         }
 
         private void DisplayHeader()
@@ -20,6 +23,25 @@ namespace ConsoleApp2
             Console.WriteLine("========                             By Derick Omondi                             ========");
             Console.WriteLine("==========================================================================================\n");
             Console.Write("Press enter to begin > ");
+            Console.ReadLine();
+        }
+
+        private void AskMeasurments()
+        {
+            AskWeight();
+            AskHeight();
+
+        }
+
+        private void AskWeight()
+        {
+            Console.Write("\nWhat is your current weight?\n>");
+            Console.ReadLine();
+        }
+
+        private void AskHeight()
+        {
+            Console.Write("\nWhat is your current height\n>");
             Console.ReadLine();
         }
     }
