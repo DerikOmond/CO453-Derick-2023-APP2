9d72abb96adc9040d50446df3729ce894651aa41
DerikOmond <derik.omond@gmail.com>
2023-03-06 03:09:22 +0000

    Input according to type
    The user can specify the units they will input their values. This is  prompted when they are asked to input the values.

--- ConsoleApp2/ConsoleApp2/BodyMassIndex.cs
+++ ConsoleApp2/ConsoleApp2/BodyMassIndex.cs
@@ -25,13 +25,20 @@ namespace ConsoleApp2
         private int weightInPounds;
         private int weightInStone;
 
+        private string weightName;
+
         private double height;
 
         private double heightInCentimetres;
         private int heightInFeet;
         private int heightInInches;
 
+        private string heightName;
+
         private double BMI;
+
+        private double option1;
+        private double option2;
 
         private bool correctOption1;
         private bool correctOption2;
@@ -39,7 +46,11 @@ namespace ConsoleApp2
         {
             DisplayHeader();
             AskMeasurments();
+            SetUnits();
+            InputData();
             CalculateBMI();
+
+            Console.WriteLine($"\nYour BMI: {BMI}");
 
 
             Console.ReadLine();
@@ -58,15 +69,16 @@ namespace ConsoleApp2
         private void AskMeasurments()
         {
             UserOptions(WEIGHT);
-            AskWeight();
+            AskWeightUnit();
             UserOptions(HEIGHT);
-            AskHeight();
+            AskHeightUnit();
 
         }
 
         private void UserOptions(string measurement)
         {
-            if(measurement.Equals(WEIGHT))
+            Console.WriteLine("\n-----------------------------------------------------------------------");
+            if (measurement.Equals(WEIGHT))
             {
                 Console.WriteLine($"\n1. {KILOGRAMS}");
                 Console.WriteLine($"2. {POUNDS}");
@@ -81,28 +93,29 @@ namespace ConsoleApp2
 
         }
 
-        private void AskWeight()
+        private void AskWeightUnit()
         {
             do
             {
                 correctOption1 = false;
-                Console.Write("\nWhich unit of measurement would you like to use?\n> ");
+                Console.Write($"\nWhich unit of measurement would you like to use for your {WEIGHT}?\n> ");
                 weight = Convert.ToInt16(Console.ReadLine());
 
                 if(weight < 1 || weight >3) 
                 {
-                    Console.WriteLine("\nTHAT IS NOT AN OPTOIN!\nTRY AGAIN.\n");
+                    Console.WriteLine("\n\nTHAT IS NOT AN OPTOIN!\nTRY AGAIN.\n");
                     correctOption1 |= true;
                 }
             }
             while (correctOption1);
         }
 
-        private void AskHeight()
+        private void AskHeightUnit()
         {
             do
             {
-                Console.Write("\nWhat is your current height in centimetres\n> ");
+                correctOption2 = false;
+                Console.Write($"\nSelect the unit of measurement you would like to use for your {HEIGHT}\n> ");
                 height = Convert.ToDouble(Console.ReadLine());
 
                 if (height < 1 || height > 3)
@@ -114,43 +127,119 @@ namespace ConsoleApp2
             while (correctOption2);
         }
 
+        private void InputData()
+        {
+            Console.Write($"\nWhat is your weight in {weightName}?\n> ");
+            double substitute = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write($"\nWhat is your height in {heightName}?\n> ");
+            Console.ReadLine();
+        }
+        
         private void CalculateBMI()
         {
             if (weight == 1 && height == 1)
             {
+                weightName = KILOGRAMS;
+                heightName = CENTIMETRES;
                 BMI = weightInKilograms / Math.Pow((heightInCentimetres/100), 2);
             }
             else if (weight == 1 && height == 2)
             {
+                weightName = KILOGRAMS;
+                heightName = FEET;
                 BMI = weightInKilograms / Math.Pow((heightInFeet * 0.3048), 2);
             }
             else if (weight == 1 && height == 3)
             {
+                weightName = KILOGRAMS;
+                heightName = INCHES;
                 BMI = weightInKilograms / Math.Pow((heightInInches * 0.0254), 2);
             }
             else if (weight == 2 && height == 1)
             {
+                weightName = POUNDS;
+                heightName = CENTIMETRES;
                 BMI = (weightInPounds * 0.453592) / Math.Pow((heightInCentimetres / 100), 2);
             }
             else if (weight == 2 && height == 2)
             {
+                weightName = POUNDS;
+                heightName = FEET;
                 BMI = (weightInPounds * 0.453592) / Math.Pow((heightInFeet * 0.3048), 2);
             }
             else if (weight == 2 && height == 3)
             {
+                weightName = POUNDS;
+                heightName = INCHES;
                 BMI = (weightInPounds * 0.453592) / Math.Pow((heightInInches * 0.0254), 2);
             }
             else if (weight == 3 && height == 1)
             {
+                weightName = STONE;
+                heightName = CENTIMETRES;
                 BMI = (weightInStone * 6.35029) / Math.Pow((heightInCentimetres / 100), 2);
             }
             else if (weight == 3 && height == 2)
             {
+                weightName = STONE;
+                heightName = FEET;
                 BMI = (weightInStone * 6.35029) / Math.Pow((heightInFeet * 0.3048), 2);
             }
             else if (weight == 3 && height == 3)
             {
+                weightName = STONE;
+                heightName = INCHES;
                 BMI = (weightInStone * 6.35029) / Math.Pow((heightInInches * 0.0254), 2);
+            }
+        }
+
+        private void SetUnits()
+        {
+            if (weight == 1 && height == 1)
+            {
+                weightName = KILOGRAMS;
+                heightName = CENTIMETRES;
+            }
+            else if (weight == 1 && height == 2)
+            {
+                weightName = KILOGRAMS;
+                heightName = FEET;
+            }
+            else if (weight == 1 && height == 3)
+            {
+                weightName = KILOGRAMS;
+                heightName = INCHES;
+            }
+            else if (weight == 2 && height == 1)
+            {
+                weightName = POUNDS;
+                heightName = CENTIMETRES;
+            }
+            else if (weight == 2 && height == 2)
+            {
+                weightName = POUNDS;
+                heightName = FEET;
+            }
+            else if (weight == 2 && height == 3)
+            {
+                weightName = POUNDS;
+                heightName = INCHES;
+            }
+            else if (weight == 3 && height == 1)
+            {
+                weightName = STONE;
+                heightName = CENTIMETRES;
+            }
+            else if (weight == 3 && height == 2)
+            {
+                weightName = STONE;
+                heightName = FEET;
+            }
+            else if (weight == 3 && height == 3)
+            {
+                weightName = STONE;
+                heightName = INCHES;
             }
         }
 
